d911f2c59f86b0fd0fab79e194211d0bf284f6a5
majsylw <58003896+majsylw@users.noreply.github.com>
2020-08-15 16:23:59 +0200

    Create gnlse python module
    Co-Authored-By: Szymon Datko <szymon.datko@pwr.edu.pl>

---
+++ setup.py
@@ -0,0 +1,30 @@
+import pkg_resources
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt', 'r') as fh:
+    reqs = [str(requirement)
+            for requirement in pkg_resources.parse_requirements(fh)]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gnlse',
+    version='1.0.0',
+    url='https://github.com/WUST-FOG/gnlse-python',
+    author='Pawłowski, A., Redman, P., Szulc, D., Zatorska, M., '
+           'Majchrowska, S., Tarnowski, K.',
+    description='gnlse-python is a Python set of scripts for solving '
+                'Generalized Nonlinear Schrodringer Equation',
+    long_description=long_description,
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=reqs,
+)
