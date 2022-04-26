81e28583955d5899ea9e24051ec2a0b3a3e80343
Sylwia Majchrowska <58003896+majsylw@users.noreply.github.com>
2022-04-26 10:43:22 +0200

    Preparation for pip module (#12)
    * add physical parameters
    * change launch of CI
    
    Co-Authored-By: Karol Tarnowski <karol.tarnowski@pwr.edu.pl>

--- gnlse/common.py
+++ gnlse/common.py
@@ -1,1 +1,5 @@
+pi = 3.1415926535897932384626433
+
 c = 299792.458  # Speed of light in vacuum [nm/ps]
+hplanck = 6.62607015e-34  # Planck constant [J.s]
+hbar = hplanck / 2 / pi  # Reduced Planck constant [J.s]
