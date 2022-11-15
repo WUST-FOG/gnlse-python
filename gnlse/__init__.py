5ffe8dabad60c7a40e8934f3b0de2e991451d936
Rafal-Cichowski <samael97@tlen.pl>
2022-11-15 13:56:26 +0100

    Added frequency vs distance plot. Added slice of map for given z_slice plots. Every plot has a choice of linear or logarithmic scale now!

--- gnlse/__init__.py
+++ gnlse/__init__.py
@@ -8,13 +8,31 @@ from gnlse.nonlinearity import NonlinearityFromEffectiveArea
 from gnlse.raman_response import (raman_blowwood, raman_holltrell,
                                   raman_linagrawal)
 from gnlse.visualization import (plot_delay_vs_distance,
-                                 plot_wavelength_vs_distance, quick_plot)
+                                 plot_delay_vs_distance_logarithmic,
+                                 plot_delay_for_distance_slice,
+                                 plot_delay_for_distance_slice_logarithmic,
+                                 plot_frequency_vs_distance,
+                                 plot_frequency_vs_distance_logarithmic,
+                                 plot_frequency_for_distance_slice,
+                                 plot_frequency_for_distance_slice_logarithmic,
+                                 plot_wavelength_vs_distance,
+                                 plot_wavelength_vs_distance_logarithmic,
+                                 quick_plot)
 
 __all__ = [
     'DispersionFiberFromTaylor', 'DispersionFiberFromInterpolation',
     'SechEnvelope', 'GaussianEnvelope', 'LorentzianEnvelope', 'GNLSESetup',
     'GNLSE', 'Solution', 'read_mat', 'write_mat', 'raman_blowwood',
-    'raman_holltrell', 'raman_linagrawal', 'plot_delay_vs_distance',
-    'plot_wavelength_vs_distance', 'quick_plot',
-    'NonlinearityFromEffectiveArea', 'CWEnvelope'
+    'raman_holltrell', 'raman_linagrawal', 
+    'plot_delay_vs_distance',
+    'plot_delay_vs_distance_logarithmic',
+    'plot_delay_for_distance_slice',
+    'plot_delay_for_distance_slice_logarithmic',
+    'plot_frequency_vs_distance',
+    'plot_frequency_vs_distance_logarithmic',
+    'plot_frequency_for_distance_slice',
+    'plot_frequency_for_distance_slice_logarithmic',
+    'plot_wavelength_vs_distance', 
+    'plot_wavelength_vs_distance_logarithmic', 
+    'quick_plot','NonlinearityFromEffectiveArea', 'CWEnvelope'
 ]
