801e440490ca770a363596b50a5ef5cf12813901
Rafal-Cichowski <samael97@tlen.pl>
2022-11-30 15:14:35 +0100

    Plot functions and example of their use.

--- gnlse/__init__.py
+++ gnlse/__init__.py
@@ -7,17 +7,20 @@ from gnlse.import_export import read_mat, write_mat
 from gnlse.nonlinearity import NonlinearityFromEffectiveArea
 from gnlse.raman_response import (raman_blowwood, raman_holltrell,
                                   raman_linagrawal)
-from gnlse.visualization import (plot_delay_vs_distance,
-                                 plot_delay_vs_distance_logarithmic,
-                                 plot_delay_for_distance_slice,
-                                 plot_delay_for_distance_slice_logarithmic,
-                                 plot_frequency_vs_distance,
-                                 plot_frequency_vs_distance_logarithmic,
-                                 plot_frequency_for_distance_slice,
-                                 plot_frequency_for_distance_slice_logarithmic,
-                                 plot_wavelength_vs_distance,
-                                 plot_wavelength_vs_distance_logarithmic,
-                                 quick_plot)
+from gnlse.visualization import (
+    plot_delay_vs_distance,
+    plot_delay_vs_distance_logarithmic,
+    plot_delay_for_distance_slice,
+    plot_delay_for_distance_slice_logarithmic,
+    plot_frequency_vs_distance,
+    plot_frequency_vs_distance_logarithmic,
+    plot_frequency_for_distance_slice,
+    plot_frequency_for_distance_slice_logarithmic,
+    plot_wavelength_vs_distance,
+    plot_wavelength_vs_distance_logarithmic,
+    plot_wavelength_for_distance_slice,
+    plot_wavelength_for_distance_slice_logarithmic,
+    quick_plot)
 
 __all__ = [
     'DispersionFiberFromTaylor', 'DispersionFiberFromInterpolation',
@@ -34,5 +37,7 @@ __all__ = [
     'plot_frequency_for_distance_slice_logarithmic',
     'plot_wavelength_vs_distance',
     'plot_wavelength_vs_distance_logarithmic',
+    'plot_wavelength_for_distance_slice',
+    'plot_wavelength_for_distance_slice_logarithmic',
     'quick_plot', 'NonlinearityFromEffectiveArea', 'CWEnvelope'
 ]
